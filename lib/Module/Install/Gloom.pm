935d0b475cd8835999c73d1dc2313bf8eedb792e
Ingy dot Net <ingy@ingy.net>
2010-09-09 17:25:02 -0700

    Check is_admin in M::I plugin

--- lib/Module/Install/Gloom.pm
+++ lib/Module/Install/Gloom.pm
@@ -12,6 +12,8 @@ sub use_gloom {
     my $module = shift
         or die "use_gloom requires the name of a target module";
 
+    return unless $self->is_admin;
+
     my $gloom_path = $self->admin->find_in_inc('Gloom') or return;
     open IN, $gloom_path or die "Can't open '$gloom_path' for input";
     my $code = do { local $/; <IN> };
