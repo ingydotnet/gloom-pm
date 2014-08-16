624cbb046019565c7daa4342989221d22e2b0f0d
Ingy döt Net <ingy@ingy.net>
2014-08-16 11:11:21 -0700

    CPAN Release 0.24
    - Meta 0.0.2

--- lib/Gloom.pm
+++ lib/Gloom.pm
@@ -7,7 +7,7 @@ use 5.008003;
 use strict;
 use warnings;
 
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 use constant XXX_skip => 1;
 
--- lib/Module/Install/Gloom.pm
+++ lib/Module/Install/Gloom.pm
@@ -1,6 +1,6 @@
 use strict; use warnings;
 package Module::Install::Gloom;
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 use Module::Install::Base;
 
